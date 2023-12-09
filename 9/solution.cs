990f0816513098ef84fd3f514a7ebf2a4697a5ff
Martin Beneš <martin.benes@stratox.com>
2023-12-09 19:14:12 +0900

    09: solve first part

---
+++ 9/solution.cs
@@ -0,0 +1,67 @@
+#r "System.Runtime.Numerics.dll"
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Numerics;
+
+class Program
+{
+    public static void Main()
+    {
+        string filePath = "input";
+
+        // Read all lines from the file BigIntegero an array
+        string[] lines = File.ReadAllLines(filePath);
+
+        List<List<BigInteger>> sequences = new List<List<BigInteger>>();
+
+        foreach (string line in lines)
+        {
+            sequences.Add(ParseLine(line));
+        }
+
+        BigInteger sum = 0;
+
+        foreach (List<BigInteger> sequence in sequences)
+        {
+            sum += Extrapolate(sequence);
+        }
+
+        Console.WriteLine($"Solution to the first part: {sum}");
+
+    }
+
+    static List<BigInteger> ParseLine(string line)
+    {
+        List<BigInteger> sequence = new List<BigInteger>();
+
+        string[] numbers = line.Split(' ');
+
+        foreach (string number in numbers)
+        {
+            sequence.Add(BigInteger.Parse(number));
+        }
+
+        return sequence;
+    }
+
+    static BigInteger Extrapolate(List<BigInteger> sequence)
+    {
+        if (sequence.TrueForAll(x => x == 0))
+        {
+            return 0;
+        }
+
+        List<BigInteger> newSequence = new List<BigInteger>();
+        for (int i = 1; i < sequence.Count; i++)
+        {
+            newSequence.Add(sequence[i] - sequence[i - 1]);
+        }
+
+        return Extrapolate(newSequence) + sequence[sequence.Count - 1];
+    }
+}
+
+Program.Main();
