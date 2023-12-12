11f575889ae3d38354781c1658a167a0ab5c4613
Martin Beneš <martin.benes@stratox.com>
2023-12-13 00:17:42 +0900

    12: solver first part, little dumb

--- 12/solution.cs
+++ 12/solution.cs
@@ -10,13 +10,22 @@ class Program
 {
     public static void Main()
     {
-        string filePath = "test";
+        string filePath = "input";
 
         string[] lines = File.ReadAllLines(filePath);
         string[] rows;
         List<int>[] values;
         ParseInput(lines, out rows, out values);
 
+        BigInteger sum = 0;
+
+        for (int i = 0; i < lines.Length; ++i)
+        {
+            int n = NumOfArrangements(rows[i], values[i]);
+            sum += n;
+        }
+
+        Console.WriteLine($"Solution of the first part: {sum}");
     }
 
     static void ParseInput(string[] lines, out string[] rows, out List<int>[] values)
@@ -39,7 +48,72 @@ class Program
 
     static int NumOfArrangements(string row, List<int> toAssign)
     {
-        return 0;
+        Dictionary<string, Dictionary<int, int>> memo = new Dictionary<string, Dictionary<int, int>>();
+
+        return NumOfArrangementsRec(row, toAssign, memo);
+    }
+
+    static int NumOfArrangementsRec(string row, List<int> toAssign, Dictionary<string, Dictionary<int, int>> memo)
+    {
+        if (memo.ContainsKey(row) && memo[row].ContainsKey(toAssign.Count()))
+        {
+            return memo[row][toAssign.Count()];
+        }
+        if (toAssign.Count() == 0)
+        {
+            if (row.Contains("#"))
+                return 0;
+            return 1;
+        }
+        else if (row.Length == 0)
+        {
+            return 0;
+        }
+        int index;
+        int result;
+        if (row.Contains("?"))
+        {
+           index = row.IndexOf("?"); 
+           string withDot = row.Substring(0, index) + "." + row.Substring(index + 1);
+           string withHash = row.Substring(0, index) + "#" + row.Substring(index + 1);
+           result = NumOfArrangementsRec(withDot, toAssign, memo) + NumOfArrangementsRec(withHash, toAssign, memo);
+           if (!memo.ContainsKey(row))
+           {
+               memo.Add(row, new Dictionary<int, int>());
+           }
+           memo[row][toAssign.Count()] = result;
+           return result;
+        }
+        int assigned = toAssign[0];
+        index = row.IndexOf("#");
+        if (index == -1 || index + assigned > row.Length)
+        {
+            if (!memo.ContainsKey(row))
+            {
+                memo.Add(row, new Dictionary<int, int>());
+            }
+            memo[row][toAssign.Count()] = 0;
+            return 0;
+        }
+        string s = row.Substring(index, assigned);
+        if (s.Contains(".") || (row.Length > index + assigned && row[index + assigned] == '#'))
+        {
+            if (!memo.ContainsKey(row))
+            {
+                memo.Add(row, new Dictionary<int, int>());
+            }
+            memo[row][toAssign.Count()] = 0;
+            return 0;
+        }
+        string newRow = index + assigned + 1 >= row.Length ? "" : row.Substring(index + assigned + 1);
+        List<int> newToAssign = toAssign.Skip(1).ToList();
+        result = NumOfArrangementsRec(newRow, newToAssign, memo);
+        if (!memo.ContainsKey(row))
+        {
+            memo.Add(row, new Dictionary<int, int>());
+        }
+        memo[row][toAssign.Count()] = result;
+        return result;
     }
 
 }
